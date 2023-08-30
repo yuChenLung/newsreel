f8b687d35553348a5f803db89db176a7ea03d057
Yu-Chen Lung <yuchen.lung1@gmail.com>
2023-08-30 00:39:55 -0400

    Basic navigation and pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:newsreel/pages/homepage.dart';
+import 'package:newsreel/pages/searchpage.dart';
+import 'package:newsreel/pages/settingspage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +19,66 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomePage(title: 'Home Page'),
+      home: const MainPage(title: 'Home Page'),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  var currPage = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget page;
+    switch (currPage) {
+      case 0:
+        page = HomePage();
+        break;
+      case 1:
+        page = SearchPage();
+        break;
+      case 2:
+        page = SettingsPage();
+        break;
+      default:
+        throw UnimplementedError('no widget for $currPage');
+    }
+
+    return Scaffold(
+      // NavBar Video: https://www.youtube.com/watch?v=FEvYl8Mzsxw
+      bottomNavigationBar: GNav(
+        onTabChange: (index) {
+          setState(() {
+            currPage = index;
+          });
+        },
+        gap: 8,
+        tabs: const [
+          GButton(
+            icon: Icons.home,
+            text: 'Home',
+          ),
+          GButton(
+            icon: Icons.search,
+            text: 'Search',
+          ),
+          GButton(
+            icon: Icons.settings,
+            text: 'Settings',
+          ),
+        ],
+      ),
+      body: Center(
+        child: page,
+      ),
     );
   }
 }
--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:google_nav_bar/google_nav_bar.dart';
-// import 'package:newsreel/main.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key, required this.title});
-  final String title;
+  const HomePage({super.key});
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -14,30 +11,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      // NavBar Video: https://www.youtube.com/watch?v=FEvYl8Mzsxw
-      bottomNavigationBar: GNav(
-        tabs: [
-          GButton(
-            icon: Icons.home,
-          ),
-          GButton(
-            icon: Icons.search,
-          ),
-          GButton(
-            icon: Icons.settings,
-          ),
-        ],
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'NewsReel',
-            ),
-          ],
-        ),
-      ),
+      body: Center(child: Text('NewsReel')),
     );
   }
 }
