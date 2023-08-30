11c535ced3adbd19f4e5e82d48b8fb10db916eeb
Yu-Chen Lung <yuchen.lung1@gmail.com>
2023-08-29 23:00:04 -0400

    test commit

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomePage(title: 'Home'),
+      home: const HomePage(title: 'Home Page'),
     );
   }
 }
